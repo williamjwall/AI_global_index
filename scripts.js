339f52a4f012b9a9dbe950f763f9c27474662d1d
William Wall <williamjwall@gmail.com>
2023-10-26 17:56:40 -0700

    scripts,css,html

--- scripts.js
+++ scripts.js
@@ -1,185 +1,278 @@
 google.charts.load('current', {
   'packages':['corechart', 'geochart'],
-  'mapsApiKey':'AIzaSyCBYaGF8mL8Dh92AGSfwBgHjkDephxDSrs'
+  'mapsApiKey':'API_KEY'
 });
 
 google.charts.setOnLoadCallback(drawRegionsMap);
+google.charts.setOnLoadCallback(drawLineChart);
+google.charts.setOnLoadCallback(drawCorrChart);
+google.charts.setOnLoadCallback(drawChart);
+// google.charts.setOnLoadCallback(drawPieChart);
 
-var country_data = [];
+var countries_obj = {};
+
+var country_score = [];
+var publication_count = []
 
 d3.csv("data/AI_index_db.csv").then(function(data) {
+  data.forEach(function(row) {
+    var country = row['Country'];
+    var totalScore = parseFloat(row['Total score']);
+    var talent = parseFloat(row['Talent']);
+    var infrastructure = parseFloat(row['Infrastructure']);
+    var operatingEnvironment = parseFloat(row['Operating Environment']);
+    var research = parseFloat(row['Research']);
+    var development = parseFloat(row['Development']);
+    var governmentStrategy = parseFloat(row['Government Strategy']);
+    var commercial = parseFloat(row['Commercial']);
+    var politicalRegime = row['Political regime'];
 
-  var country_obj = {};
+    country_score.push([country, totalScore]);
 
-  var countries;
-
-  for (i = 0; i < data.length; i++) {
-    var country = data[i]['Country'];
-    var totalScore = parseFloat(data[i]['Total score']);
-
-    if (country in country_obj) {
-      country_obj[country] += totalScore;
-    } else {
-      country_obj[country] = totalScore;
-    }
-  }
-
-  countries = Object.keys(country_obj);
-
-  for (i = 0; i < countries.length; i++) {
-    var totalScore = country_obj[countries[i]];
-    var curr_country = countries[i];
-    var new_obj = [curr_country, totalScore];
-    country_data.push(new_obj);
-  }
-
-  country_data = country_data.sort(function(a, b) {
-    return b[1] - a[1];
+    countries_obj[country] = {
+      totalScore: totalScore,
+      talent: talent,
+      infrastructure: infrastructure,
+      operatingEnvironment: operatingEnvironment,
+      research: research,
+      development: development,
+      governmentStrategy: governmentStrategy,
+      commercial: commercial,
+      politicalRegime: politicalRegime
+    };
   });
-  country_data.unshift(['Country', 'Total Score']);
 
-  // Call the drawRegionsMap function after processing the data
-  drawRegionsMap();
+}).catch(function(error) {
+  console.error("Error loading the CSV file:", error);
 });
 
-function drawRegionsMap() {
-  var country_data = [];
-
-  d3.csv("data/AI_index_db.csv").then(function(data) {
-
-    var country_obj = {};
-
-    var countries;
-
-    for (i = 0; i < data.length; i++) {
-      var country = data[i]['Country'];
-      var totalScore = parseFloat(data[i]['Total score']);
-
-      if (country in country_obj) {
-        country_obj[country] += totalScore;
-      } else {
-        country_obj[country] = totalScore;
-      }
-    }
-
-    countries = Object.keys(country_obj);
-
-    for (i = 0; i < countries.length; i++) {
-      var totalScore = country_obj[countries[i]];
-      var curr_country = countries[i];
-      var new_obj = [curr_country, totalScore];
-      country_data.push(new_obj);
-    }
-
-    country_data = country_data.sort(function(a, b) {
-      return b[1] - a[1];
-    });
-    country_data.unshift(['Country', 'Total Score']);
-
-    var chartData = google.visualization.arrayToDataTable(country_data);
-
-    var options = {
-      title: 'AI Global Index by Country',
-      colorAxis: {
-        colors: ['#4374e0', '#a4c7f4']
-      }
-    };
-
-    var chart = new google.visualization.GeoChart(document.getElementById('regionsmap'));
-
-    chart.draw(chartData, options);
-  });
-}
-
-function draw_investment_by_company(country) {
-  var curr_data = company_investment_per_country[country];
-
-  try {
-      if (curr_data.length === 1) {
-          //console.log(curr_data.length)
-          return;
-      }
-  } catch(TypeError) {
-      return;
-  }
-
-  // Extract the necessary columns from curr_data
-  var extractedData = curr_data.map(function(row) {
-      return [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]];
-  });
-
-  var data = google.visualization.arrayToDataTable(extractedData);
+function drawLineChart() {
+  // arrayToDataTable wasn't working had to specify data types for some reason as 'numbers'
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'Year');
+  data.addColumn('number', 'Publications');
+  data.addRows([
+    [2010, 162444],
+    [2011, 170168],
+    [2012, 176611],
+    [2013, 183656],
+    [2014, 192310],
+    [2015, 194000],
+    [2016, 194194],
+    [2017, 206392],
+    [2018, 237482],
+    [2019, 281659],
+    [2020, 311675],
+    [2021, 334497],
+    [2022, 350000],
+  ]);
 
   var options = {
-      title: `Top Companies in ${country} by China's Total Investments Since 2005 (Millions)`,
-      hAxis: {
-          title: "Company",
+    title: 'AI Publications Over Time',
+    hAxis: { title: 'Year', format: '####' },
+    vAxis: {
+      title: 'Publications',
+      viewWindow: {
+        min: 1000,
+        max: 375000,
       },
-      vAxis: {
-          title: "Investments (Millions)"
-      },
-      allowHtml: true,
-      bar: {groupWidth: "30%"},
-      legend: { position: "top" },
-      animation:{
-          startup: true,
-          duration: 1000,
-          easing: 'out',
-      },
-      colors:['red'],
+    },
+    legend: 'top',
+    animation: { startup: true, duration: 1000, easing: 'out' },
+    colors: ['#0a4d19'],
+    curveType: 'function',
+    pointSize: 6,
   };
 
-  var chart = new google.visualization.ColumnChart(document.getElementById("investment_by_country_chart"));
+  var chart = new google.visualization.LineChart(document.getElementById('linechart'));
   chart.draw(data, options);
-
-  country_column_drawn = true;
-  curr_country_for_column = country;
 }
 
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['AI Library', 'Number of Cumulative GitHub Stars'],
+    ['TensorFlow', 160.672],
+    ['Keras', 53.218],
+    ['OpenCV', 58.561],
+    ['DeepLearning-500-questions', 46.651],
+    ['TenserFlow-Examples', 41.461],
+    ['Scikit-learn', 47.991],
+    ['Pytorch', 52.745],
+  ]);
 
+  var options = {
+    chartArea: { width: '50%' },
+    title: 'Number of Cumulative GitHub Stars (in thousands)',
+    colors:['green'],
+    legend: 'none'
+  };
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
 
+function drawCorrChart() {
 
+  var chartContainer = document.getElementById('barChart_corr');
 
+  // This conditional statement checks if chartContainer is falsy, which means the element with the ID 'barChart_corr' wasn't found.
+  if (!chartContainer) {
+    console.error("Container for the chart is not defined.");
+    return;
+  }
 
+  var data = google.visualization.arrayToDataTable([
+    ['Category', 'Score'],
+    ['Talent', 0.861969],
+    ['Infrastructure', 0.716481],
+    ['Operating Environment', 0.368566],
+    ['Research', 0.945877],
+    ['Development', 0.866337],
+    ['Government Strategy', 0.531821],
+    ['Commercial', 0.857985]
+  ]);
+
+  var options = {
+    title: 'Correlation with Total Score',
+    chartArea: { width: '50%' },
+    hAxis: {
+      minValue: 0,
+    },
+
+    colors:['#0a4d19']
+  };
+
+  var chart = new google.visualization.BarChart(chartContainer);
+  chart.draw(data, options);
+}
+
+// function drawPieChart() {
+//   // count occurrences
+//   var regimeCounts = {};
+
+//   country_score.forEach(function ([country, totalScore]) {
+//     var politicalRegime = countries_obj[country].politicalRegime;
+//     regimeCounts[politicalRegime] = (regimeCounts[politicalRegime] || 0) + 1;
+//   });
+
+//   var pieData = Object.entries(regimeCounts).map(([regime, count]) => ({ regime, count }));
+
+//   var width = 300;
+//   var height = 300;
+//   var radius = Math.min(width, height) / 2;
+
+//   var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+//   var svg = d3.select("body").append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//     .append("g")
+//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+//   var pie = d3.pie()
+//     .value(function (d) { return d.count; });
+
+//   var path = d3.arc()
+//     .outerRadius(radius - 10)
+//     .innerRadius(0);
+
+//   var arc = svg.selectAll("arc")
+//     .data(pie(pieData))
+//     .enter().append("g")
+//     .attr("class", "arc");
+
+//   arc.append("path")
+//     .attr("d", path)
+//     .attr("fill", function (d) { return color(d.data.regime); });
+
+//   arc.append("text")
+//     .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
+//     .attr("dy", ".4em")
+//     .text(function (d) { return d.data.regime + " (" + d.data.count + ")"; });
+// }
 
 function initMap(){
   //Prevents API console error for some reason
 }
 
 function drawRegionsMap() {
-  var data = google.visualization.arrayToDataTable(country_data);
+  try{
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Country');
+  data.addColumn('number', 'Total Score');
+  data.addRows(country_score);
 
   var options = {
-      legend: {textStyle: {color: 'black', fontSize: 16}},
-      colorAxis: {colors: ['white', '#f76565', 'red']},
+    legend: {textStyle: {color: 'black', fontSize: 16}},
+    colorAxis: {colors: ['white', '#285224', 'green']},
   };
 
   var chart = new google.visualization.GeoChart(document.getElementById('countryChart'));
 
   chart.draw(data, options);
 
-  function click_func(){
-      var selection = chart.getSelection();
-      try {
-          var row = parseInt(selection[0]['row']);
-      } catch(TypeError) {
-          return;
-      }
-      var country = country_data[row + 1][0];
-      draw_investment_by_company(country);
-      draw_sector_by_country(country);
+function click_func() {
+    var selection = chart.getSelection();
+    try {
+      var row = parseInt(selection[0]['row']);
+
+    } catch (TypeError) {
+      return;
+    }
+    var country = country_score[row][0];
+
+    drawBarChart(country);
   }
   google.visualization.events.addListener(chart, 'select', click_func);
+} catch (error) {
+  console.error("Error drawing regions map:", error);
+  }
 }
 
+function drawBarChart(selectedCountry) {
+  var countryData = countries_obj[selectedCountry];
+
+  if (!countryData) {
+    console.error(`Data for ${selectedCountry} is undefined.`);
+    return;
+  }
+
+  var barChartData = [
+    ['Category', 'Score'],
+    ['Talent', countryData.talent],
+    ['Infrastructure', countryData.infrastructure],
+    ['Operating Environment', countryData.operatingEnvironment],
+    ['Research', countryData.research],
+    ['Development', countryData.development],
+    ['Government Strategy', countryData.governmentStrategy],
+    ['Commercial', countryData.commercial]
+  ];
+
+  var data = google.visualization.arrayToDataTable(barChartData);
+
+  var options = {
+    title: `Scores for ${selectedCountry} (${countryData.politicalRegime})`,
+
+    legend: { position: "none" },
+    animation: {
+      startup: true,
+      duration: 1000,
+      easing: 'out',
+    },
+    colors:['green'],
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('barChart'));
+
+  chart.draw(data, options);
+}
 
 function resize() {
-  if (document.getElementById('investment_by_country_chart').innerHTML === "") {
-      document.getElementById('investment_by_country_chart').innerHTML = "<h1 style='height: 50vh;float:left;margin-top:300px;margin-left:150px;'>Click a Country</h1>"
-  }
   drawRegionsMap();
   drawBarChart();
-
+  drawLineChart();
+  drawCorrChart();
+  // drawPieChart()
 }
 
 window.onload = resize;
